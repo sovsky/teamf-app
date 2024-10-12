7ffbc3324f1a0cf2880444ba03277c54c4af5c03
HurasAdam <huradam@wp.pl>
2024-10-12 17:35:52 +0200

    feat: -Added Hero section component.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar:React.FC = () => {
         <a href="#" className='py-2 px-3 border rounded-md'>
             Zaloguj
         </a>
-        <a href="#" className='bg-blue-500 text-neutral-50 font-semibold py-2 px-4 rounded-md'>
+        <a href="#" className='bg-teal-500 text-neutral-50 font-semibold py-2 px-4 rounded-md'>
 Dołącz
         </a>
     </div>
@@ -66,7 +66,7 @@ Dołącz
 </ul>
 <div className="flex space-x-6 " >
     <a href="#" className='py-2 px-3 border rounded-md'>Zaloguj</a>
-    <a href="#" className='bg-blue-500 py-2 px-3 rounded-md text-neutral-50 font-semibold'>
+    <a href="#" className='bg-teal-500 py-2 px-3 rounded-md text-neutral-50 font-semibold'>
 Dołącz
         </a>
 </div>

1f174207191861fa972bc2e4525cebbaf4c96119
Aneta <aneta.gloc@gmail.com>
2024-10-16 15:40:33 +0200

    I installed Swagger (L5-Swagger) with the initial configuration

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -1,6 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+/**
+ * @OA\Info(
+ *     title="API Dokumentacja Twojej Aplikacji",
+ *     version="1.0.0",
+ *     description="Dokumentacja API aplikacji"
+ * ),
+ * @OA\Server(
+ *     url="http://loacalhost/api",
+ *     description="Główna ścieżka API"
+ * )
+ */
 
 abstract class Controller
 {

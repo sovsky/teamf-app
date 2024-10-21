a405322bba0176d3c49063e4ec68b0a1273ccb3d
Aneta <aneta.gloc@gmail.com>
2024-10-21 21:45:35 +0200

    I added error handling for each field in RegisterController

--- backend/app/Http/Controllers/API/RegisterController.php
+++ backend/app/Http/Controllers/API/RegisterController.php
@@ -16,20 +16,55 @@ class RegisterController extends BaseController
      */
     public function register(Request $request): JsonResponse
     {
+        //validate fields
         $validator = Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required|email',
             'password' => 'required',
-            'c_password' => 'required|same:password',
+            'confirmed_password' => 'required|same:password',
+            'age' => 'required|date', 
+            'phone_number' => 'required|string|regex:/^([0-9\s\-\+\(\)]*)$/|min:10', 
+            'city' => 'required|string|max:100'
         ]);
-        if($validator->fails()){
-            return $this->sendError('Validation Error.', $validator->errors());       
+        if ($validator->fails()) {
+            $errors = [];
+
+            // error handling for each field
+            if ($validator->errors()->has('name')) {
+                $errors['name'] = 'Name is required and should be a valid string with a maximum length of 255 characters.';
+            }
+
+            if ($validator->errors()->has('email')) {
+                $errors['email'] = 'A valid email is required and it must be unique.';
+            }
+
+            if ($validator->errors()->has('password')) {
+                $errors['password'] = 'Password is required and should have a minimum length of 6 characters.';
+            }
+
+            if ($validator->errors()->has('c_password')) {
+                $errors['confirmed_password'] = 'Confirm password is required and must match the password.';
+            }
+
+            if ($validator->errors()->has('age')) {
+                $errors['age'] = 'Age is required, must be an date.';
+            }
+
+            if ($validator->errors()->has('phone_number')) {
+                $errors['phone_number'] = 'Phone number is required, must be at least 10 characters long, and should follow a valid format.';
+            }
+
+            if ($validator->errors()->has('city')) {
+                $errors['city'] = 'City is required and must be a valid string with a maximum length of 100 characters.';
+            }
+
+            return $this->sendError('Validation Error.', $errors);
         }
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
         $success['token'] =  $user->createToken('AppTeamF')->plainTextToken;
-        $success['name'] =  $user->name;
+        $success['email'] =  $user->email;
         return $this->sendResponse($success, 'User register successfully.');
     }
     /**
@@ -42,7 +77,16 @@ class RegisterController extends BaseController
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
             $user = Auth::user(); 
             $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
-            $success['name'] =  $user->name;
+            $success['email'] =  $user->email;
+
+            if (!$success['token']) {
+                return $this->sendError('Missing token.', ['error' => 'Missing token.']);
+            }
+            
+            if (!$success['email']) {
+                return $this->sendError('Missing name.', ['error' => 'Missing email.']);
+            }
+
             return $this->sendResponse($success, 'User login successfully.');
         }else{ 
             return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);

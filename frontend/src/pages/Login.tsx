a19c10c38fe3c4acc02dcd7a56b4ab7ff13c3fc4
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2024-10-21 22:24:58 +0200

    feat: add page transition with framer motion
    - create FormsLayout.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,16 +5,14 @@ import LoginForm from "@/components/forms/login/loginForm.tsx";
 export default function Login() {
 
     return (
-        <main className="h-screen flex justify-center items-center bg-violet-50 p-5">
-            <Card className="sm:px-10 sm:py-5 min-w-[300px] w-full max-w-[600px]">
-                <CardHeader>
-                    <Link className="text-blue-400 font-semibold py-2" to="/">Strona Główna</Link>
-                    <CardTitle className="text-2xl font-bold">Witaj z powrotem</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <LoginForm/>
-                </CardContent>
-            </Card>
-        </main>
+        <Card className="sm:px-10 sm:py-5 min-w-[300px] w-full max-w-[600px]">
+            <CardHeader>
+                <Link className="text-blue-400 font-semibold py-2" to="/">Strona Główna</Link>
+                <CardTitle className="text-2xl font-bold">Witaj z powrotem</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <LoginForm/>
+            </CardContent>
+        </Card>
     )
 }

8c3d7c0858fa1f5498b6371f6e1352fccba28faa
Aneta <aneta.gloc@gmail.com>
2024-11-05 16:01:14 +0100

    I added the ability to create accounts for admin, secure paths for admin and I added admin paths documentation

--- backend/app/Http/Controllers/API/UserController.php
+++ backend/app/Http/Controllers/API/UserController.php
@@ -6,64 +6,20 @@ namespace App\Http\Controllers\API;
 use Validator;
 use App\Models\Role;
 use App\Models\User;
+use App\Http\Controllers\API\BaseController as BaseController;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use OpenApi\Attributes as OA;
-use OpenApi\Attributes\Schema;
 use Illuminate\Validation\Rule;
-use OpenApi\Attributes\Property;
 use Illuminate\Http\JsonResponse;
-use OpenApi\Attributes\MediaType;
 use Illuminate\Support\Facades\Log;
-use OpenApi\Attributes\RequestBody;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
-use App\Http\Controllers\API\BaseController as BaseController;
-
-#[OA\Post(
-    path: "/api/register",
-    summary: "Registration of platform users",
-    requestBody: new RequestBody(required: true,
-            content: new MediaType(mediaType: "application/json",
-            schema: new Schema(required: ["name", "email", "password", "password_confirm", "age", "phone_number", "city"],
-                    properties: [
-                        new Property(property: 'name', description: "User name must be max 255 characters", type: "string"),
-                        new Property(property: 'email', description: "User email must be unique", type: "string"),
-                        new Property(property: 'password', description: "User password, at least 8 characters with mixed cases, numbers, and symbols", type: "string"),
-                        new OA\Property(property: 'password_confirm', description: "User password confirmation", type: "string"),
-                        new Property(property: 'age', description: "User age mus be a date type, in example: 1990-01-01", type: "datetime"),
-                        new Property(property: 'phone_number', description: "User phone number in the format +1234567890", type: "string", pattern: "^[0-9\\s\\-\\+\\(\\)]*$"),
-                        new OA\Property(property: 'city', description: "User city must be max 100 characters", type: "string"),
-                    ]))),
-    tags: ["Users"],
-    responses: [
-        new OA\Response(response: Response::HTTP_CREATED, description: "User register successfully."),
-        new OA\Response(response: Response::HTTP_UNPROCESSABLE_ENTITY, description: "Unprocessable entity"),
-        new OA\Response(response: Response::HTTP_BAD_REQUEST, description: "Bad Request"),
-        new OA\Response(response: Response::HTTP_INTERNAL_SERVER_ERROR, description: "Server Error")
-    ]
-)]
-
-#[OA\Post(
-    path: "/api/login",
-    summary: "Login of platform users",
-    requestBody: new OA\RequestBody(required: true,
-            content: new OA\MediaType(mediaType: "application/x-www-form-urlencoded",
-            schema: new OA\Schema(required: [ "email", "password"],
-                    properties: [
-                        new OA\Property(property: 'email', description: "User email", type: "string"),
-                        new OA\Property(property: 'password', description: "User password", type: "string"),
-                    ]))),
-    tags: ["Users"],
-    responses: [
-        new OA\Response(response: Response::HTTP_CREATED, description: "User login successfully."),
-        new OA\Response(response: Response::HTTP_UNPROCESSABLE_ENTITY, description: "Unprocessable entity"),
-        new OA\Response(response: Response::HTTP_BAD_REQUEST, description: "Bad Request"),
-        new OA\Response(response: Response::HTTP_INTERNAL_SERVER_ERROR, description: "Server Error")
-    ]
-)]
-
+use OpenApi\Attributes as OA;
+use OpenApi\Attributes\Schema;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\MediaType;
+use OpenApi\Attributes\RequestBody;
 
 class UserController extends BaseController
 {
@@ -72,6 +28,29 @@ class UserController extends BaseController
      *
      * @return \Illuminate\Http\Response
      */
+    #[OA\Post(
+        path: "/api/register",
+        summary: "Registration of platform users",
+        requestBody: new RequestBody(required: true,
+                content: new MediaType(mediaType: "application/json",
+                schema: new Schema(required: ["name", "email", "password", "password_confirm", "age", "phone_number", "city"],
+                        properties: [
+                            new Property(property: 'name', description: "User name must be max 255 characters", type: "string"),
+                            new Property(property: 'email', description: "User email must be unique", type: "string"),
+                            new Property(property: 'password', description: "User password, at least 8 characters with mixed cases, numbers, and symbols", type: "string"),
+                            new OA\Property(property: 'password_confirm', description: "User password confirmation", type: "string"),
+                            new Property(property: 'age', description: "User age mus be a date type, in example: 1990-01-01", type: "datetime"),
+                            new Property(property: 'phone_number', description: "User phone number in the format +1234567890", type: "string", pattern: "^[0-9\\s\\-\\+\\(\\)]*$"),
+                            new OA\Property(property: 'city', description: "User city must be max 100 characters", type: "string"),
+                        ]))),
+        tags: ["Users"],
+        responses: [
+            new OA\Response(response: Response::HTTP_CREATED, description: "User register successfully."),
+            new OA\Response(response: Response::HTTP_UNPROCESSABLE_ENTITY, description: "Unprocessable entity"),
+            new OA\Response(response: Response::HTTP_BAD_REQUEST, description: "Bad Request"),
+            new OA\Response(response: Response::HTTP_INTERNAL_SERVER_ERROR, description: "Server Error")
+        ]
+    )]
     public function register(Request $request): JsonResponse
     {
         // Validate fields
@@ -108,6 +87,29 @@ class UserController extends BaseController
                          ->cookie('token', $token, 60 * 24, '/', null, false, true, false);
     }
 
+    /**
+     * Login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    #[OA\Post(
+        path: "/api/login",
+        summary: "Login of platform users",
+        requestBody: new OA\RequestBody(required: true,
+                content: new OA\MediaType(mediaType: "application/x-www-form-urlencoded",
+                schema: new OA\Schema(required: [ "email", "password"],
+                        properties: [
+                            new OA\Property(property: 'email', description: "User email", type: "string"),
+                            new OA\Property(property: 'password', description: "User password", type: "string"),
+                        ]))),
+        tags: ["Users"],
+        responses: [
+            new OA\Response(response: Response::HTTP_CREATED, description: "User login successfully."),
+            new OA\Response(response: Response::HTTP_UNPROCESSABLE_ENTITY, description: "Unprocessable entity"),
+            new OA\Response(response: Response::HTTP_BAD_REQUEST, description: "Bad Request"),
+            new OA\Response(response: Response::HTTP_INTERNAL_SERVER_ERROR, description: "Server Error")
+        ]
+    )]
     public function login(Request $request): JsonResponse
     {
         $validated = $request->validate([
@@ -143,13 +145,24 @@ class UserController extends BaseController
     }
 
 
-
+    /**
+     * Logout api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    #[OA\Post(
+        path: "/api/logout",
+        summary: "Logout of platform users",
+        tags: ["Users"],
+        responses: [
+            new OA\Response(response: Response::HTTP_OK, description: "User logged out successfully."),
+        ]
+    )]
     public function logout(Request $request): JsonResponse
     {
         // Auth::logout();
         $request->user()->currentAccessToken()->delete();
         return response()->json(['message' => 'User logged out successfully.'], Response::HTTP_OK);
     }
-
     
 }

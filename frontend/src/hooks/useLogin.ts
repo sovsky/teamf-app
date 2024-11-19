66393128360c015be246221802ea719c4e6bab5f
https://github.com/HurasAdam <huradam@wp.pl>
2024-11-19 22:11:24 +0100

    Feat: Added login handling with API integration.

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -1,14 +1,17 @@
 import {useMutation} from "@tanstack/react-query";
 import login from "@/lib/api/login.ts";
 import useAuth from "@/hooks/useAuth.ts";
+import { useNavigate } from "react-router-dom";
 
 export default function useLogin() {
+const navigate = useNavigate();
 
     const {setUserWithStorage} = useAuth()
     const {mutate, status, error} = useMutation({
-        mutationFn: login,
+        mutationFn: (values)=>login(values),
         onSuccess: (data) => {
-            setUserWithStorage(data.data.data)
+            setUserWithStorage({email:"email@email.com",token:"12345"})
+            navigate('/')
         }
     })
 
--- frontend/src/lib/api/login.ts
+++ frontend/src/lib/api/login.ts
@@ -2,9 +2,7 @@ import API from "@/config/api.client.ts";
 import {LoginInputs} from "@/components/forms/login/loginForm.tsx";
 
 export default function login(value: LoginInputs) {
-    return API.request({
-        url: "/api/login",
-        method: "POST",
-        data: value
-    })
+    console.log(value)
+    return API.post("/login",value)
+  
 }

3047f323c402edeb4b90a060b073a1697dac6084
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2024-11-25 01:58:07 +0100

    fix: change url in getUser function

--- frontend/src/lib/api/getUser.ts
+++ frontend/src/lib/api/getUser.ts
@@ -3,7 +3,7 @@ import API from "@/config/api.client.ts";
 export default function getUser(token?: string) {
     return API.request({
         method: "GET",
-        url: "/api/user",
+        url: "/user",
         headers: {
             Authorization: `Bearer ${token}`
         }
